0321ecf9730f540b76c1e78cf7831beed587b57d
Aditya <97450298+1234-ad@users.noreply.github.com>
2025-10-02 16:06:12 +0530

    Add ThemeProvider to app root for dark mode support

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,12 +1,13 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { Analytics } from "@vercel/analytics/react"
+import { ThemeProvider } from "../lib/ThemeContext"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </ThemeProvider>
   )
 }
