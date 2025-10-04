73caeaf98a53a68d1a53ecde3e5f8a5dff209c25
Ankit <work.ankitpokhrel@gmail.com>
2025-10-04 19:45:41 +0545

    added dark-light theme

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "../../components/**/*.{html,js,jsx,ts,tsx}"
+    "./contexts/**/*.{js,ts,jsx,tsx}",
+    "../../components/**/*.{html,js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
